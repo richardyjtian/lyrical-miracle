2aa9170b28022b1eb27570d84a48a885df02f5c8
yyonata <yyonata@sfu.ca>
2021-01-09 22:14:48 -0800

    working on music player and styling

---
+++ lyrical-client/src/components/Play/AlbumCover.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AlbumCover.css';
+
+class AlbumCover extends React.Component{
+  render(){
+      return(
+        <div className="AlbumCover">
+          <img src="https://i.pinimg.com/originals/b4/75/00/b4750046d94fed05d00dd849aa5f0ab7.jpg" width="128" height="128"></img>
+        </div>
+      )   
+  }
+
+}export default AlbumCover;
--- lyrical-client/src/components/Play/MusicPlayer.js
+++ lyrical-client/src/components/Play/MusicPlayer.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import IconButton from '@material-ui/core/IconButton';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import './MusicPlayer.css';
 
 class MusicPlayer extends React.Component{
   render(){
       return(
-        <h2> MUSIC PLAYER HERE</h2>
+        <div>
+          <Box  m={1} border={1} borderColor="white" borderRadius={16} className="musicPlayerBox">
+            <IconButton aria-label="Shuffle" className="Shuffle">
+              <ShuffleIcon />
+            </IconButton>  
+            <IconButton aria-label="SkipPrevious" className="SkipPrevious">
+              <SkipPreviousIcon fontSize="large"/>
+            </IconButton>
+            <IconButton aria-label="PlayPause" className="PlayPause">
+              <PlayCircleOutlineIcon fontSize="large"/>
+            </IconButton>
+            <IconButton aria-label="SkipNext" className="SkipNext">
+              <SkipNextIcon fontSize="large"/>
+            </IconButton>
+            <IconButton aria-label="Repeat" className="Repeat">
+              <RepeatIcon/>
+            </IconButton>          
+          </Box>
+        </div>
       )   
   }
 
